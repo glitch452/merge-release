1596e8f3e191554aec01bd63de285b992b39b6fe
glitch452 <894128+glitch452@users.noreply.github.com>
2024-04-16 16:20:48 -0700

    chore: Added the `.prettierrc` config file

--- README.md
+++ README.md
@@ -1,57 +1,58 @@
 # ![LogoMakr-2ULBLV](https://github.com/Github-Actions-Community/merge-release/assets/3071208/bb7d9b4c-04bd-41c5-9c08-0ee5c91fa4a1)
 
 <!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+
 [![All Contributors](https://img.shields.io/badge/all_contributors-4-orange.svg?style=flat-square)](#contributors-)
+
 <!-- ALL-CONTRIBUTORS-BADGE:END -->
 
 GitHub Action for automated npm publishing.
 
-This Action publishes a package to npm. It is meant to be used on every successful merge to main but 
+This Action publishes a package to npm. It is meant to be used on every successful merge to main but
 you'll need to configure that workflow yourself. You can look to the
 [`.github/workflows/push.yml`](./.github/workflows/release.yml) file in this project as an example.
 
 ### Workflow
 
-* Check for the latest version number published to npm.
-* Lookup all commits between the git commit that triggered the action and the latest publish.
-  * If the package hasn't been published or the prior publish does not include a git hash, we'll
+- Check for the latest version number published to npm.
+- Lookup all commits between the git commit that triggered the action and the latest publish.
+  - If the package hasn't been published or the prior publish does not include a git hash, we'll
     only pull the commit data that triggered the action.
-* Based on the commit messages, increment the version from the lastest release.
-  * If the strings "BREAKING CHANGE" or "!:" are found anywhere in any of the commit messages or descriptions the major 
+- Based on the commit messages, increment the version from the lastest release.
+  - If the strings "BREAKING CHANGE" or "!:" are found anywhere in any of the commit messages or descriptions the major
     version will be incremented.
-  * If a commit message begins with the string "feat" then the minor version will be increased. This works
+  - If a commit message begins with the string "feat" then the minor version will be increased. This works
     for most common commit metadata for feature additions: `"feat: new API"` and `"feature: new API"`.
-  * All other changes will increment the patch version.
-* Publish to npm using the configured token.
-* Push a tag for the new version to GitHub.
-
+  - All other changes will increment the patch version.
+- Publish to npm using the configured token.
+- Push a tag for the new version to GitHub.
 
 ### Configuration
 
 You can configure some aspects of merge-release action by passing some environmental variables.
 
-* **GITHUB_TOKEN (required)**
-  * Github token to allow tagging the version.
-* **NPM_AUTH_TOKEN (required)**
-  * NPM Auth Token to publish to NPM, read [here](https://docs.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets) how to setup it as a secret.
-* **DEPLOY_DIR**
-  * The path where the dist `package.json` is to run npm publish. Defaults to the root dir.
-* **SRC_PACKAGE_DIR**
-  * The path where the src package.json is found. Defaults to the root dir.
-* **NPM_REGISTRY_URL**
-  * NPM Registry URL to use. defaults to: `https://registry.npmjs.org/`
-* **NPM_PRIVATE**
-  * If you wish privately publish your package please ensure you have set this to `true`
+- **GITHUB_TOKEN (required)**
+  - Github token to allow tagging the version.
+- **NPM_AUTH_TOKEN (required)**
+  - NPM Auth Token to publish to NPM, read [here](https://docs.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets) how to setup it as a secret.
+- **DEPLOY_DIR**
+  - The path where the dist `package.json` is to run npm publish. Defaults to the root dir.
+- **SRC_PACKAGE_DIR**
+  - The path where the src package.json is found. Defaults to the root dir.
+- **NPM_REGISTRY_URL**
+  - NPM Registry URL to use. defaults to: `https://registry.npmjs.org/`
+- **NPM_PRIVATE**
+  - If you wish privately publish your package please ensure you have set this to `true`
 
 `merge-release` will use `npm publish` unless you've defined a `publish` script in your `package.json`.
 
 ```yaml
 - uses: Github-Actions-Community/merge-release@main
   env:
-      GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-      NPM_AUTH_TOKEN: ${{ secrets.NPM_AUTH_TOKEN }}
-      DEPLOY_DIR: my/deploy/dir
-      SRC_PACKAGE_DIR: my/src/package
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    NPM_AUTH_TOKEN: ${{ secrets.NPM_AUTH_TOKEN }}
+    DEPLOY_DIR: my/deploy/dir
+    SRC_PACKAGE_DIR: my/src/package
 ```
 
 ## Contributors ✨
@@ -79,4 +80,4 @@ Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/d
 
 This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
 
-##### Created my free [logo](https://logomakr.com/5sISSS) at [LogoMakr.com](LogoMakr.com) 
+##### Created my free [logo](https://logomakr.com/5sISSS) at [LogoMakr.com](LogoMakr.com)
--- src/merge-release-run.js
+++ src/merge-release-run.js
@@ -1,104 +1,119 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-const bent = require('bent')
-const git = require('simple-git')()
-const { execSync, spawnSync } = require('child_process')
-const { promisify } = require('util')
+const fs = require('fs');
+const path = require('path');
+const bent = require('bent');
+const git = require('simple-git')();
+const { execSync, spawnSync } = require('child_process');
+const { promisify } = require('util');
 
 const exec = (str, cwd) => {
-  const [cmd, ...args] = str.split(' ')
-  const ret = spawnSync(cmd, args, { cwd, stdio: 'inherit' })
+  const [cmd, ...args] = str.split(' ');
+  const ret = spawnSync(cmd, args, { cwd, stdio: 'inherit' });
   if (ret.status) {
-    console.error(ret)
-    console.error(`Error: ${str} returned non-zero exit code`)
-    process.exit(ret.status)
+    console.error(ret);
+    console.error(`Error: ${str} returned non-zero exit code`);
+    process.exit(ret.status);
   }
-  return ret
-}
+  return ret;
+};
 
-const getlog = promisify(git.log.bind(git))
+const getlog = promisify(git.log.bind(git));
 
-const get = bent('json', process.env.NPM_REGISTRY_URL || 'https://registry.npmjs.org/')
+const get = bent('json', process.env.NPM_REGISTRY_URL || 'https://registry.npmjs.org/');
 
-const event = JSON.parse(fs.readFileSync('/github/workflow/event.json').toString())
+const event = JSON.parse(fs.readFileSync('/github/workflow/event.json').toString());
 
-const deployDir = path.join(process.cwd(), process.env.DEPLOY_DIR || './')
-const srcPackageDir = path.join(process.cwd(), process.env.SRC_PACKAGE_DIR || './')
+const deployDir = path.join(process.cwd(), process.env.DEPLOY_DIR || './');
+const srcPackageDir = path.join(process.cwd(), process.env.SRC_PACKAGE_DIR || './');
 
-console.log('            using deploy directory : ' + deployDir)
-console.log('using src directory (package.json) : ' + srcPackageDir)
+console.log('            using deploy directory : ' + deployDir);
+console.log('using src directory (package.json) : ' + srcPackageDir);
 
-const access = process.env.NPM_PRIVATE === 'true' ? 'restricted' : 'public'
+const access = process.env.NPM_PRIVATE === 'true' ? 'restricted' : 'public';
 
-console.log('deploy to NPM with access : ' + access)
+console.log('deploy to NPM with access : ' + access);
 
-let pkg = require(path.join(deployDir, 'package.json'))
+let pkg = require(path.join(deployDir, 'package.json'));
 
 const run = async () => {
-  if (!process.env.NPM_AUTH_TOKEN) throw new Error('Merge-release requires NPM_AUTH_TOKEN')
-  let latest
+  if (!process.env.NPM_AUTH_TOKEN) throw new Error('Merge-release requires NPM_AUTH_TOKEN');
+  let latest;
   try {
-    latest = await get(pkg.name + '/latest')
+    latest = await get(pkg.name + '/latest');
   } catch (e) {
     // unpublished
   }
 
-  let messages
+  let messages;
 
   if (latest) {
-    if (latest.gitHead === process.env.GITHUB_SHA) return console.log('SHA matches latest release, skipping.')
+    if (latest.gitHead === process.env.GITHUB_SHA) return console.log('SHA matches latest release, skipping.');
     if (latest.gitHead) {
       try {
-        let logs = await getlog({ from: latest.gitHead, to: process.env.GITHUB_SHA })
-        messages = logs.all.map(r => r.message + '\n' + r.body)
+        let logs = await getlog({
+          from: latest.gitHead,
+          to: process.env.GITHUB_SHA,
+        });
+        messages = logs.all.map((r) => r.message + '\n' + r.body);
       } catch (e) {
-        latest = null
+        latest = null;
       }
       // g.log({from: 'f0002b6c9710f818b9385aafeb1bde994fe3b370', to: '53a92ca2d1ea3c55977f44d93e48e31e37d0bc69'}, (err, l) => console.log(l.all.map(r => r.message + '\n' + r.body)))
     } else {
-      latest = null
+      latest = null;
     }
   }
   if (!latest) {
-    messages = (event.commits || []).map(commit => commit.message + '\n' + commit.body)
+    messages = (event.commits || []).map((commit) => commit.message + '\n' + commit.body);
   }
 
-  let version = 'patch'
-  if (messages.map(message => message.includes('BREAKING CHANGE') || message.includes('!:')).includes(true)) {
-    version = 'major'
-  } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
-    version = 'minor'
+  let version = 'patch';
+  if (messages.map((message) => message.includes('BREAKING CHANGE') || message.includes('!:')).includes(true)) {
+    version = 'major';
+  } else if (messages.map((message) => message.toLowerCase().startsWith('feat')).includes(true)) {
+    version = 'minor';
   }
 
-  const setVersion = version => {
-    const json = execSync(`jq '.version="${version}"' package.json`, { cwd: srcPackageDir })
-    fs.writeFileSync(path.join(srcPackageDir, 'package.json'), json)
+  const setVersion = (version) => {
+    const json = execSync(`jq '.version="${version}"' package.json`, {
+      cwd: srcPackageDir,
+    });
+    fs.writeFileSync(path.join(srcPackageDir, 'package.json'), json);
 
     if (deployDir !== './') {
-      const deployJson = execSync(`jq '.version="${version}"' package.json`, { cwd: deployDir })
-      fs.writeFileSync(path.join(deployDir, 'package.json'), deployJson)
+      const deployJson = execSync(`jq '.version="${version}"' package.json`, {
+        cwd: deployDir,
+      });
+      fs.writeFileSync(path.join(deployDir, 'package.json'), deployJson);
     }
-  }
+  };
 
-  let currentVersion = execSync(`npm view ${pkg.name} version`, { cwd: srcPackageDir }).toString()
-  setVersion(currentVersion)
-  console.log('current:', currentVersion, '/', 'version:', version)
-  let newVersion = execSync(`npm version --git-tag-version=false ${version}`, { cwd: srcPackageDir }).toString()
-  newVersion = newVersion.trim().split(/(\r\n|\n|\r)/gm).filter(word => !/(\r\n|\n|\r)/.test(word)).pop()
-  setVersion(newVersion.slice(1))
-  console.log('new version:', newVersion)
+  let currentVersion = execSync(`npm view ${pkg.name} version`, {
+    cwd: srcPackageDir,
+  }).toString();
+  setVersion(currentVersion);
+  console.log('current:', currentVersion, '/', 'version:', version);
+  let newVersion = execSync(`npm version --git-tag-version=false ${version}`, {
+    cwd: srcPackageDir,
+  }).toString();
+  newVersion = newVersion
+    .trim()
+    .split(/(\r\n|\n|\r)/gm)
+    .filter((word) => !/(\r\n|\n|\r)/.test(word))
+    .pop();
+  setVersion(newVersion.slice(1));
+  console.log('new version:', newVersion);
 
   if (pkg.scripts && pkg.scripts.publish) {
-    exec(`npm run publish`, deployDir)
+    exec(`npm run publish`, deployDir);
   } else {
-    exec(`npm publish --access=${access}`, deployDir)
+    exec(`npm publish --access=${access}`, deployDir);
   }
-  exec(`git checkout ${path.join(deployDir, 'package.json')}`) // cleanup
-  exec(`git tag ${newVersion}`)
-  exec(`echo "version=${newVersion}" >> $GITHUB_OUTPUT`)
+  exec(`git checkout ${path.join(deployDir, 'package.json')}`); // cleanup
+  exec(`git tag ${newVersion}`);
+  exec(`echo "version=${newVersion}" >> $GITHUB_OUTPUT`);
 
-  const remote = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
-  exec(`git push ${remote} --tags`)
-}
-run()
+  const remote = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+  exec(`git push ${remote} --tags`);
+};
+run();
